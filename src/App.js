437d1abc36856554ed9f77713cf48a5c150a9702
윤종협 <99409757+hyobbeee@users.noreply.github.com>
2023-04-12 18:24:15 +0900

    dev branch commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { useState } from 'react';
 
+const StyledBtn = styled.button`
+  background-color: skyblue;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  font-weight: bold;
+  margin: 30px;
+`;
 function App() {
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => setCount(count + 1);
+  const handleDecrement = () => setCount(count - 1);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div className="countbox">
+          <StyledBtn onClick={handleIncrement}>+</StyledBtn>
+          <span>{count}</span>
+          <StyledBtn onClick={handleDecrement}>-</StyledBtn>
+        </div>
       </header>
     </div>
   );
